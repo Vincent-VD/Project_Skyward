f06cd6a24e157a0f279ab5b32140d306b3b20418
Vincent VD <vincent.vd@icloud.com>
2025-11-13 15:27:08 +0100

    Add player movement
    In OnMove, store context.performed to execture movement logic in FixedUpdate. OnMove is only called when an action is started and when it ends, so the logic in FixedUpdate functions more like a toggle.

--- Project_Skyward/Assets/Scripts/Player/PlayerBase.cs
+++ Project_Skyward/Assets/Scripts/Player/PlayerBase.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerBase : MonoBehaviour
 {
+	[SerializeField] private float _moveSpeed = -5.0f;
 	[SerializeField] private float _cooldown = 1.0f;
 	[SerializeField] private SpriteRenderer _spriteRenderer;
 	[SerializeField] private float _animFrameTime = 0.2f;
@@ -16,10 +18,17 @@ public class PlayerBase : MonoBehaviour
 	private float _currCooldown = .0f;
 	private int _animFrame = 0;
 	private float _currAnimeFrameTime = 0.0f;
+	
+	private Transform _playerTransform;
+	private bool _shouldMove = false;
+	private Vector2 _moveVec = Vector2.zero;
     
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
+		// Get player Transform component
+		_playerTransform = GetComponent<Transform>();
+		
 		// Get player input component and enable current action map
 		_playerInput = GetComponent<PlayerInput>();
 		InputSystem.actions.Disable();
@@ -29,6 +38,11 @@ public class PlayerBase : MonoBehaviour
 		_moveStateMap.Add(MoveTypes.MoveState.Idle, "Idle");
 		_moveStateMap.Add(MoveTypes.MoveState.Move, "Move");
 		_moveStateMap.Add(MoveTypes.MoveState.Attack, "Attack");
+		
+		// Asserts
+		Assert.IsNotNull(_playerTransform);
+		Assert.IsNotNull(_playerInput.currentActionMap);
+		Assert.AreEqual(_playerInput.currentActionMap?.name, "player");
 	}
 
 	void Update()
@@ -55,16 +69,23 @@ public class PlayerBase : MonoBehaviour
 	public void OnMove(InputAction.CallbackContext context)
 	{
 		var input = context.ReadValue<Vector2>();
+		
+		// Movement
+		_shouldMove = context.performed;
+		_moveVec = input;
 
+		// Check if movement should be processed
 		float absX = Mathf.Abs(input.x);
-		 float absY = Mathf.Abs(input.y);
+		float absY = Mathf.Abs(input.y);
 		if (absX < 0.05f & absY < 0.05f)
 		{
 			_moveState = MoveTypes.MoveState.Idle;
 			_animFrame = 0;
+			_shouldMove = false;
 			return;
 		}
 		
+		// Sprite logic
 		MoveTypes.MoveDir moveDir = MoveTypes.MoveDir.None;
 		
 		// k = tan(22.5°) ≈ 0.4142
@@ -91,5 +112,14 @@ public class PlayerBase : MonoBehaviour
 		
 		Sprite newSprite = SpriteManager.Instance.GetPlayerSprite(_moveState, _moveDir, _animFrame);
 		_spriteRenderer.sprite = newSprite;
+	}
+
+	private void FixedUpdate()
+	{
+		if (_shouldMove)
+		{
+			Vector2 moveVec = _moveVec * (Time.fixedDeltaTime * _moveSpeed);
+			GetComponent<Transform>().position += new Vector3(-moveVec.y, 0.0f, moveVec.x);
+		}
 	}
 }
