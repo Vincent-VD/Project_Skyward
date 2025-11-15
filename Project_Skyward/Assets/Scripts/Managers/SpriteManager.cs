f97353780b06170eec0c59efe5e69075cfadf86a
Vincent VD <vincent.vd@icloud.com>
2025-11-15 14:30:42 +0100

    Added Manager namespace to SpriteManager

--- Project_Skyward/Assets/Scripts/Managers/SpriteManager.cs
+++ Project_Skyward/Assets/Scripts/Managers/SpriteManager.cs
@@ -2,66 +2,69 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public class SpriteManager : MonoBehaviour
+namespace Managers
 {
-	public static SpriteManager Instance { get; private set; }
-	
-	[SerializeField] private DefaultAsset _playerAttackRoot = null;
-	[SerializeField] private DefaultAsset _playerIdleRoot = null;
-	[SerializeField] private DefaultAsset _playerMoveRoot = null;
-    
-	private Sprite[][] _playerAssets = new Sprite[(int)MoveTypes.MoveState.Count][];
-
-	private void Awake()
+	public class SpriteManager : MonoBehaviour
 	{
-		if (Instance != null && Instance != this)
-		{
-			Destroy(this);
-		}
-		else
-		{
-			Instance = this;
-			Instance.Init();
-		}
-		
-		DontDestroyOnLoad(this);
-	}
+		public static SpriteManager Instance { get; private set; }
 
-	private void Init()
-	{
-		//Player Sprite init
-		Sprite[] attackSprites = GetSpritesFromFolder(_playerAttackRoot);
-		Sprite[] idleSprites = GetSpritesFromFolder(_playerIdleRoot);
-		Sprite[] moveSprites = GetSpritesFromFolder(_playerMoveRoot);
+		[SerializeField] private DefaultAsset _playerAttackRoot = null;
+		[SerializeField] private DefaultAsset _playerIdleRoot = null;
+		[SerializeField] private DefaultAsset _playerMoveRoot = null;
 
-		_playerAssets[(int)MoveTypes.MoveState.Attack] = attackSprites;
-		_playerAssets[(int)MoveTypes.MoveState.Idle] = idleSprites;
-		_playerAssets[(int)MoveTypes.MoveState.Move] = moveSprites;
-	}
+		private Sprite[][] _playerAssets = new Sprite[(int)Structs.MoveState.Count][];
 
-	public Sprite GetPlayerSprite(MoveTypes.MoveState moveState, MoveTypes.MoveDir moveDir, int frame)
-	{
-		if (moveDir == MoveTypes.MoveDir.None)
+		private void Awake()
 		{
-			Debug.LogError("GetPlayerSprite called MoveDir.None");
-			return null;
+			if (Instance != null && Instance != this)
+			{
+				Destroy(this);
+			}
+			else
+			{
+				Instance = this;
+				Instance.Init();
+			}
+
+			DontDestroyOnLoad(this);
 		}
 
-		return _playerAssets[(int)moveState][(int)moveDir * 8 + frame];
-	}
-
-	private Sprite[] GetSpritesFromFolder(DefaultAsset assetFolder)
-	{
-		string assetPath = AssetDatabase.GetAssetPath(assetFolder);
-		string[] guids = AssetDatabase.FindAssets("t:Sprite", new[] { assetPath });
-		string[] assetPaths = guids.Select(AssetDatabase.GUIDToAssetPath).ToArray();
-		Sprite[] currState = new Sprite[assetPaths.Length];
-		for (int i = 0; i < assetPaths.Length; i++)
+		private void Init()
 		{
-			string currAsset = assetPaths[i];
-			currState[i] = AssetDatabase.LoadAssetAtPath<Sprite>(currAsset);
+			//Player Sprite init
+			Sprite[] attackSprites = GetSpritesFromFolder(_playerAttackRoot);
+			Sprite[] idleSprites = GetSpritesFromFolder(_playerIdleRoot);
+			Sprite[] moveSprites = GetSpritesFromFolder(_playerMoveRoot);
+
+			_playerAssets[(int)Structs.MoveState.Attack] = attackSprites;
+			_playerAssets[(int)Structs.MoveState.Idle] = idleSprites;
+			_playerAssets[(int)Structs.MoveState.Move] = moveSprites;
 		}
 
-		return currState;
+		public Sprite GetPlayerSprite(Structs.MoveState moveState, Structs.MoveDir moveDir, int frame)
+		{
+			if (moveDir == Structs.MoveDir.None)
+			{
+				Debug.LogError("GetPlayerSprite called MoveDir.None");
+				return null;
+			}
+
+			return _playerAssets[(int)moveState][(int)moveDir * 8 + frame];
+		}
+
+		private Sprite[] GetSpritesFromFolder(DefaultAsset assetFolder)
+		{
+			string assetPath = AssetDatabase.GetAssetPath(assetFolder);
+			string[] guids = AssetDatabase.FindAssets("t:Sprite", new[] { assetPath });
+			string[] assetPaths = guids.Select(AssetDatabase.GUIDToAssetPath).ToArray();
+			Sprite[] currState = new Sprite[assetPaths.Length];
+			for (int i = 0; i < assetPaths.Length; i++)
+			{
+				string currAsset = assetPaths[i];
+				currState[i] = AssetDatabase.LoadAssetAtPath<Sprite>(currAsset);
+			}
+
+			return currState;
+		}
 	}
 }
