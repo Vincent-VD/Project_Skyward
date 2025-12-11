e8432902beb4f70f35e2d1e9b624486783a30068
Vincent VD <vincent.vd@icloud.com>
2025-12-11 18:49:14 +0100

    Add BaseSpriteAnimator
    Manages the sprite animation controller parameters for game objects.
    Separate entity from animator controllers and is intended to be reusable for other enemy entities as well.

---
+++ Project_Skyward/Assets/Scripts/Animators/BaseSpriteAnimator.cs
@@ -0,0 +1,133 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Animators
+{
+	public class BaseSpriteAnimator : MonoBehaviour
+	{
+		[Serializable]
+		public struct AnimPriorityTuple
+		{
+			public Structs.BaseMoveStates moveState;
+			public int priority;
+		}
+		[SerializeField] private float _animFrameTime = 0.2f;
+		[SerializeField] private AnimPriorityTuple[] _animPriorities;
+		[SerializeField] private Animator _animator;
+    
+		private SpriteRenderer _spriteRenderer;
+
+		private Structs.MoveDir _moveDir = Structs.MoveDir.Right;
+		private Structs.BaseMoveStates _moveState = Structs.BaseMoveStates.Idle;
+	
+		private Structs.BaseMoveStates _prevMoveState = Structs.BaseMoveStates.Idle;
+    
+		private int _animFrame = 0;
+		private float _currAnimeFrameTime = 0.0f;
+    
+		void Start()
+		{
+			// Get sprite renderer component
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_animator.SetInteger("MoveState", (int)_moveState);
+			_animator.SetInteger("MoveDir", (int)_moveDir);
+		
+			Assert.NotNull(_spriteRenderer, "Sprite renderer not found, make sure it's a component of the same object");
+
+			Assert.That(_animFrameTime > 0f);
+		}
+
+		// Update animation frames based on anim frame time
+		void Update()
+		{
+			_currAnimeFrameTime += Time.deltaTime;
+
+			if (_currAnimeFrameTime >= _animFrameTime)
+			{
+				_animFrame = (_animFrame + 1) % 8;
+			
+				/*Sprite newSprite = Managers.SpriteManager.Instance.GetPlayerSprite(_moveState, _moveDir, _animFrame);
+				_spriteRenderer.sprite = newSprite;*/
+				_currAnimeFrameTime = 0.0f;
+			}
+		}
+
+		// Update move direction
+		public void SetMoveDir(Structs.MoveDir dir)
+		{
+			if (dir != _moveDir)
+			{
+				_animator.SetInteger("MoveDir", (int)dir);
+				Debug.Log((float)dir / (float)Structs.MoveDir.Count);
+				_animator.SetFloat("MoveDirFl", (float)dir / (float)Structs.MoveDir.Count);
+				_animator.SetTrigger("Right");
+			}
+			
+			_moveDir = dir;
+		}
+
+		public void SetMoveSpeed(Vector2 vec)
+		{
+			if (vec != Vector2.zero)
+			{
+				_animator.SetFloat("MoveX", vec.x);
+				_animator.SetFloat("MoveY", vec.y);
+			}
+			_animator.SetBool("Move", vec != Vector2.zero);
+		}
+
+		// Request animation change, will not change to lower priority animation
+		public bool RequestAnimChange(Structs.BaseMoveStates newState)
+		{
+			int newStatePriority = FindAnimPriority(newState);
+			int currStatePriority = FindAnimPriority(_moveState);
+
+			if (newStatePriority < currStatePriority) return false;
+
+			if (newState != _moveState)
+			{
+				Debug.Log("Update MoveState " + (int)newState);
+				_animator.SetInteger("MoveState", (int)newState);
+				_animator.SetTrigger("Right");
+				
+				_prevMoveState = _moveState;
+				_moveState = newState;
+				_animFrame = 0;
+				_currAnimeFrameTime = 0.0f;
+				Debug.Log((int)_prevMoveState + " - " + (int)_moveState);
+			}
+
+			return true;
+
+		}
+
+		// Will end curr state and revert to prev state if curr state is
+		//  equal to state to be ended
+		public bool RequestEndState(Structs.BaseMoveStates stateToEnd)
+		{
+			if (_moveState != stateToEnd) return false;
+			
+			Debug.Log("Revert MoveState " + (int)_moveState);
+			_animator.SetInteger("MoveState", (int)stateToEnd);
+		
+			_moveState = _prevMoveState;
+			_animFrame = 0;
+			_currAnimeFrameTime = 0.0f;
+			Debug.Log((int)_prevMoveState + " - " + (int)_moveState);
+			return true;
+		}
+
+		private int FindAnimPriority(Structs.BaseMoveStates state)
+		{
+			foreach (AnimPriorityTuple item in _animPriorities)
+			{
+				if (item.moveState == state)
+				{
+					return item.priority;
+				}
+			}
+			return -1;
+		}
+	}
+}
