6e35523dab838e9191ad47be6c0c52d377a8325a
Vincent VD <vincent.vd@icloud.com>
2025-12-17 19:01:15 +0100

    Simplify PlayerAttack behavior
    By using structs to separate state attack timing metadata.

--- Project_Skyward/Assets/Scripts/Player/PlayerAttack.cs
+++ Project_Skyward/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,4 +1,5 @@
-using Structs;
+using System;
+using Animators;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,13 +7,36 @@ namespace Player
 {
 	public class PlayerAttack : MonoBehaviour
 	{
-		[SerializeField] private float _attackCooldown = 1.0f;
-		[SerializeField] private PlayerSpriteBody _playerSpriteBody;
+		[Serializable]
+		struct AttackData
+		{
+			public Structs.BaseMoveStates moveState;
+			public float totalTime;     // total attack time
+			public float hitStart;      // start time for collision
+			public float hitEnd;        // end time for collision
+			public float linkOpen;      // earliest time a next input can be buffered
+			public float linkClose;     // latest time a next input can be buffered
+			public float attackSpeed;	// Attack animation speed multiplier
+		}
+		
+		[SerializeField] private BaseSpriteAnimator _animator;
+		[SerializeField] private StateMachine.BaseAnimationStateMachine _animationStateMachine;
+		[SerializeField, Header("Attack 1")] private AttackData _attack1Data;
+		[SerializeField, Header("Attack 2")] private AttackData _attack2Data;
+		[SerializeField, Header("Attack 3")] private AttackData _attack3Data;
+		[SerializeField, Header("Cooldown")] private float _attackCooldown;
+
 
 		private BoxCollider _attackCollider;
 
-		private Structs.AttackStates _currAttackState;
-		private float _currCooldown;
+		private AttackData _currAnimFrame;
+		private float _currAnimTime;
+		private bool _isAttacking = false;
+
+		private float _currAttackCooldown;
+		private bool _isAttackCooldownActive = false;
+
+		private float _attackSpeedFactor = 0.0f;
 
 		void Start()
 		{
@@ -22,23 +46,48 @@ namespace Player
 		// Update is called once per frame
 		void Update()
 		{
-			// Only update cooldown timer when collider is enabled
-			if (_attackCollider.enabled)
+			// Update current animation frame when attacking
+			if (_isAttacking)
 			{
-				_currCooldown += Time.deltaTime;
+				_currAnimTime += Time.deltaTime;
 
-				if (_currCooldown >= _attackCooldown)
+				// Enable attack hitbox at hitStart
+				if (_currAnimTime >= _currAnimFrame.hitStart * _attackSpeedFactor)
+				{
+					_attackCollider.enabled = true;
+				}
+				
+				// Disable attack hitbox at hitEnd
+				if (_currAnimTime >= _currAnimFrame.hitEnd * _attackSpeedFactor)
 				{
 					_attackCollider.enabled = false;
-					_currAttackState = AttackStates.None;
-					_playerSpriteBody.SetAttackState(_currAttackState);
+				}
+				
+				if (_currAnimTime >= _currAnimFrame.totalTime * _attackSpeedFactor)
+				{
+					if (_animationStateMachine.RequestEndState(_currAnimFrame.moveState))
+					{
+						_animator.SetAnimationState(_animationStateMachine.GetMoveState());
+					}
+				}
+
+				if (_currAnimTime >= _currAnimFrame.linkClose * _attackSpeedFactor)
+				{
+					_isAttacking = false;
+					_isAttackCooldownActive = true;
+					_currAnimTime = 0.0f;
 				}
 			}
-		}
+			if (_isAttackCooldownActive)
+			{
+				_currAttackCooldown += Time.deltaTime;
 
-		private void OnCollisionEnter(Collision other)
-		{
-			Debug.Log("OnCollisionEnter");
+				if (_currAttackCooldown > _attackCooldown)
+				{
+					_currAttackCooldown = 0.0f;
+					_isAttackCooldownActive = false;
+				}
+			}
 		}
 
 		public void OnAttack(InputAction.CallbackContext context)
@@ -48,48 +97,62 @@ namespace Player
 			{
 				return;
 			}
-			
-			if (_attackCollider.enabled == false)
+
+			if (_isAttackCooldownActive)
 			{
-				if (_currAttackState != Structs.AttackStates.ForceCooldown)
+				Debug.Log("Attack in cooldown");
+				return;
+			}
+
+			Structs.BaseMoveStates currState = _animationStateMachine.GetMoveState();
+			Structs.BaseMoveStates newState = Structs.BaseMoveStates.Attack1;
+
+			switch (currState)
+			{
+				case Structs.BaseMoveStates.Idle:
+				case Structs.BaseMoveStates.Move:
+				case Structs.BaseMoveStates.Jump:
 				{
-					_currCooldown = 0.0f;
-					_currAttackState = Structs.AttackStates.StandardCombo1;
-					_attackCollider.enabled = true;
+					SetStateAndAnimFrame(newState, _attack1Data);
+					break;
 				}
-				else
+				case Structs.BaseMoveStates.Attack1:
 				{
-					Debug.Log("Attack in force cooldown");
+					// If input is registered between linkOpen and linkClose
+					if (_currAnimTime >= _currAnimFrame.linkOpen &&
+					    _currAnimTime <= _currAnimFrame.linkClose)
+					{
+						SetStateAndAnimFrame(newState, _attack2Data);
+					}
+					break;
+				}
+				case Structs.BaseMoveStates.Attack2:
+				{
+					if (_currAnimTime >= _currAnimFrame.linkOpen &&
+					    _currAnimTime <= _currAnimFrame.linkClose)
+					{
+						SetStateAndAnimFrame(newState, _attack3Data);
+					}
+					break;
+				}
+				default:
+				{
+					Debug.Log("Default OnAttack state");
+					break;
 				}
 			}
-			else
+		}
+
+		private void SetStateAndAnimFrame(Structs.BaseMoveStates newState, AttackData attackData)
+		{
+			if (_animationStateMachine.RequestNewState(newState))
 			{
-				switch (_currAttackState)
-				{
-					case Structs.AttackStates.StandardCombo1:
-					{
-						Debug.Log("StandardCombo1 transition to StandardCombo2");
-						_currAttackState = Structs.AttackStates.StandardCombo2;
-						_currCooldown = 0.0f;
-						break;
-					}
-					case Structs.AttackStates.StandardCombo2:
-					{
-						Debug.Log("StandardCombo2 transition to StandardCombo3");
-						_currAttackState = Structs.AttackStates.StandardCombo3;
-						_currCooldown = 0.0f;
-						break;
-					}
-					case Structs.AttackStates.StandardCombo3:
-					{
-						Debug.Log("StandardCombo3 transition to ForceCooldown");
-						_currCooldown = 0.0f;
-						break;
-					}
-				}
+				_animator.SetAnimationState(newState);
+				_animator.SetAttackAnimationSpeed(attackData.attackSpeed);
+				_attackSpeedFactor = 1 / attackData.attackSpeed;
 			}
-			
-			_playerSpriteBody.SetAttackState(_currAttackState);
+			_currAnimFrame = attackData;
+			_isAttacking = true;
 		}
 	}
 }
