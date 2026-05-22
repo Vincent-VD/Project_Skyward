b15b892099e28e66e1dd5df0edfdc2121e318042
Vincent VD <vincent.vd@icloud.com>
2026-05-22 18:43:55 +0200

    Add StatType struct

---
+++ Project_Skyward/Assets/Scripts/Structs/StatType.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Structs
+{
+	public enum StatType
+	{
+		Health,
+		Attack,
+		Defense,
+
+		CritRate,
+		CritDmg,
+		MovementSpeed,
+
+		DebuffApplRate,
+		DebuffProcRate,
+
+		// Abs buffs/deb modify base stats for a time
+		AbsAttack,
+		AbsDefense,
+		AbsCritRate,
+		AbsCritDmg,
+
+		COUNT,
+	}
+}
